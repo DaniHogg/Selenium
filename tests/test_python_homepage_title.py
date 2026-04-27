954cfb39faa9b4360c89bb0cb3a3cca20be2b52e
DaniHogg <daniel.hogg1992@gmail.com>
2026-04-27 20:35:57 +1000

    changed fixed time sleeps to condition based waits

--- tests/test_python_homepage_title.py
+++ tests/test_python_homepage_title.py
@@ -1,8 +1,8 @@
-import time
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Test that checks the title of the Python homepage
 def test_python_homepage_title(browser):
     browser.get("https://www.python.org")
-    time.sleep(1)  # Wait for the page to load
+    WebDriverWait(browser, 10).until(EC.title_contains("Python"))
     assert "Python" in browser.title  # Assert that 'Python' is in the page title
